e329ce1630fe121152cf3d730bb4c5269ac9dc8e
taajuddin <taajraza@gmail.com>
2020-09-01 12:51:25 +0530

    strated

---
+++ client/src/index.js
@@ -0,0 +1,24 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// // import {Provider} from 'react-redux'
+// // import configureStore from './store/configureStore'
+
+// // const store=configureStore()
+
+// // const ele = (
+// //         //<Provider store = {store}>
+// //             <App/>
+// //         //</Provider>
+// // )
+// <App/>
+
+
+// ReactDOM.render( document.getElementById('root'))
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
