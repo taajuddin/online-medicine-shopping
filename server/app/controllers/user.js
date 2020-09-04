a67db4ef5591f5851db2d92d218de18dd9ff13ba
taajuddin <taajraza@gmail.com>
2020-09-04 13:05:16 +0530

    starting

--- server/app/controllers/user.js
+++ server/app/controllers/user.js
@@ -3,19 +3,18 @@ const User = require('../models/user');
 const user={}
 
 user.read=(req,res)=>{
-	req.profile.hashed_password=undefined
-	return res.json(req.profile)
+	//req.user.password=undefined
+	return res.json(req.user)
 
 }
 user.update=(req,res)=>{
-	User.findOneAndUpdate({_id:req.profile._id},{$set:req.body},{new:true},(err,user)=>{
+	User.findOneAndUpdate({_id:req.user._id},{$set:req.body},{new:true},(err,user)=>{
 		if(err){
 			return res.status(400).json({
 				error:"you are not authorized to perform this task"
 			})
 		}
-		user.hashed_password=undefined
-		user.salt=undefined
+		//user.password=undefined
 		res.json(user)
 	})
 
